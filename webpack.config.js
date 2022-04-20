75c0c0b913066d1eec7044ee8e1fcfee3f3c1bdc
zoedms <zoe.dumas@live.fr>
2022-04-20 15:53:32 +0200

    dernier changement composer/webpack/env (bdd)

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,18 @@ Encore
   // public path used by the web server to access the output path
   .setPublicPath("/build")
   .setPublicPath("/build/website")
+  .copyFiles({
+    from: "./assets/website/images",
+
+    // optional target path, relative to the output dir
+    to: "images/[path][name].[ext]",
+
+    // if versioning is enabled, add the file hash too
+    //to: 'images/[path][name].[hash:8].[ext]',
+
+    // only copy files matching this pattern
+    //pattern: /.(png|jpg|jpeg)$/
+  })
   // only needed for CDN's or sub-directory deploy
   //.setManifestKeyPrefix('build/')
 
